faa302720a93ff1c9ad9ed6adb783ae03a9c283c
Mansueto Paiva <mansuetoonofre@gmail.com>
2024-06-11 01:27:56 +0000

    aprendendo keys.js

---
+++ object/keys.js
@@ -0,0 +1,10 @@
+// Object.keys() O método estático retorna uma matriz de nomes de propriedades enumeráveis ​​com chave de string do próprio objeto.
+
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false,
+  };
+  
+  console.log(Object.keys(object1)); // Expected output: Array ["a", "b", "c"]
+  
